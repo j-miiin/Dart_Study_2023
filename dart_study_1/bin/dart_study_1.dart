52c2967ca15c170784b2f5276d7cd7d2ac7131e4
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-07-19 20:44:28 +0900

    프로젝트 생성

---
+++ dart_study_1/bin/dart_study_1.dart
@@ -0,0 +1,5 @@
+import 'package:dart_study_1/dart_study_1.dart' as dart_study_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_study_1.calculate()}!');
+}
--- dart_study_1/test/dart_study_1_test.dart
+++ dart_study_1/test/dart_study_1_test.dart
@@ -1,4 +1,4 @@
-import 'package:dart_study_0/dart_study_0.dart';
+import 'package:dart_study_1/dart_study_1.dart';
 import 'package:test/test.dart';
 
 void main() {
